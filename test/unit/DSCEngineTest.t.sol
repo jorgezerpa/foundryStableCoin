c6367846e7fe14d12ee9810f551db29ad2b7ef0b
Jorge Zerpa <jorgezerpacoder@gmail.com>
2025-03-28 09:50:26 -0400

    partial

---
+++ test/unit/DSCEngineTest.t.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+import { Test } from "forge-std/Test.sol";
+import {DeployDSC} from "script/DeployDSC.s.sol";
+import {DescentralizedStableCoin} from "src/DescentralizedStableCoin.sol";
+import {DSCEngine} from "src/DSCEngine.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+
+contract DSCEngineTest is Test {
+    DeployDSC deployer;
+    DescentralizedStableCoin dsc;
+    DSCEngine dsce;
+    HelperConfig config;
+    address ethUsdPriceFeed;
+    address weth;
+
+    address public USER = makeAddr("user");
+    uint256 public constant AMOUNT_COLLATERAL = 10 ether;
+    uint256 public constant STARTING_ERC20_BALANCE = 10 ether;
+
+    function setUp() public {
+        deployer = new DeployDSC();
+        (dsc, dsce, config) = deployer.run();
+        (ethUsdPriceFeed, weth, , ,) = config.activeNetworkConfig();
+
+        ERC20Mock(weth).mint(USER, STARTING_ERC20_BALANCE);
+    }   
+
+    ///////////////////////
+    // PRICE TESTS
+    //////////////////////
+
+    function testGetUSDValue() public view{
+        uint256 ethAmount = 15e18;
+        uint256 expectedUsd = 3000e18;
+        uint256 actualUsd = dsce.getUSDValue(weth, ethAmount);
+
+        assertEq(expectedUsd, actualUsd);
+    }
+
+    ///////////////////////
+    // DEPOSIT COLLATERAL TESTS
+    //////////////////////
+    
+    function testRevertsIfCollateralZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(dsce), AMOUNT_COLLATERAL);
+
+        vm.expectRevert(DSCEngine.DSCEngine__NeedsMoreThanZero.selector);
+        dsce.depositCollateral(weth, 0);
+        vm.stopPrank();
+    }
+
+}
